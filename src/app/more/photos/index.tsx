a86ed2658467f740e62ed1d26e6872d63691d337
unknown <netizenslive10@gmail.com>
2024-06-01 07:42:07 -0700

    removed some files

--- src/app/more/photos/index.tsx
+++ src/app/more/photos/index.tsx
@@ -1,7 +1,5 @@
 "use client";
 import Photo from "@/components/Photo";
-import Modal from "@/components/Test/Modal";
-import Image from "next/image";
 import React, { useRef, useState } from "react";
 
 const Photos = ({ photos, searchParams }) => {
@@ -13,7 +11,7 @@ const Photos = ({ photos, searchParams }) => {
 
   return (
     <>
-      {/* <div className="container">
+      <div className="container">
         <div className="border-b border-body-color/[.15] pb-16 dark:border-white/[.15] md:pb-20 lg:pb-28">
           <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4  ">
             {photos.map((photo, index) => (
@@ -27,14 +25,9 @@ const Photos = ({ photos, searchParams }) => {
             ))}
           </div>
         </div>
-      </div> */}
-
-      <div className=" flex  w-full items-center justify-center">
-        <div className=" h-lvh  w-[1300px] overflow-hidden">
-          <Modal images={photos} searchParams={searchParams}/>
-         
-        </div>
       </div>
+
+     
     </>
   );
 };
